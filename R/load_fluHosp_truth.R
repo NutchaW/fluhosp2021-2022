75beeffd7ebc7a23e0689e8dee371fecdbc964bb
Nutcha Wattanachit <nutcha.wattanachit@gmail.com>
2021-10-27 15:33:18 -0400

    commented out stuff

--- R/load_fluHosp_truth.R
+++ R/load_fluHosp_truth.R
@@ -20,7 +20,7 @@ load_fluHosp_truth <- function(
     locs <- locations
   }
   if(truth_date=NULL){
-    tdates <- sa
+    tdates <- default_dates # have to edit
   }else{
     tdates <- truth_date
   }
@@ -37,7 +37,7 @@ load_fluHosp_truth <- function(
     tar <- target_variable
   }
   if(as_of=NULL){
-    issue_date <-  some_date  # Fetch data
+    issue_date <-  default_issue_dates # have to edit
   }else{
     issue_date <- as_of
   }
@@ -48,15 +48,15 @@ load_fluHosp_truth <- function(
   return(truth_data)
 }
 
-# have to think about when the date can be set since no influenza columns last year
-res <- Epidata$covid_hosp("MA",c(20200510,20210510))
-check <- lapply(res[[1]], function(x) data.frame(t(unlist(x))))
-setdiff(colnames(check[[1]]),colnames(check[[2]]))
-
-check2 <- Epidata$covid_hosp("MA",c(20200510,20210510))[['epidata']] %>%
-  lapply(., function(x) data.frame(t(unlist(x)))) %>%
-  map_dfr(., ~ .x %>%
-            as.list %>%
-            as_tibble)
+# # have to think about when the date can be set since no influenza columns last year
+# res <- Epidata$covid_hosp("MA",c(20200510,20210510))
+# check <- lapply(res[[1]], function(x) data.frame(t(unlist(x))))
+# setdiff(colnames(check[[1]]),colnames(check[[2]]))
+# 
+# check2 <- Epidata$covid_hosp("MA",c(20200510,20210510))[['epidata']] %>%
+#   lapply(., function(x) data.frame(t(unlist(x)))) %>%
+#   map_dfr(., ~ .x %>%
+#             as.list %>%
+#             as_tibble)
 
 
